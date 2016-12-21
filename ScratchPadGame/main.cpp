8d39c39fc7ab37f308a1edecd9ec74bee2cf2bf6
DenisDubinin <dadubinin@gmail.com>
2016-12-21 18:17:15 +0100

    init

--- ScratchPadGame/main.cpp
+++ ScratchPadGame/main.cpp
@@ -1,15 +1,8 @@
-//
-//  main.cpp
-//  ScratchPadGame
-//
-//  Created by apple on 2016-12-21.
-//  Copyright © 2016 apple. All rights reserved.
-//
 
-#include <iostream>
+#include <BatGameEngine/BatGameEngine.hpp>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    BatGameEngine engine;
     return 0;
 }
